061392a5dddffe4ebb41e4ed0c17ef2115b8a6a9
김윤서 <moca_grla@gim-yunseoui-MacBookAir.local>
2024-03-22 23:10:30 +0900

    팀 이름 추가 반영

--- intro.c
+++ intro.c
@@ -1,15 +1,17 @@
 #include <stdio.h>}
 
 int main() {
-    char name[10] ;
+    char name[10], team_name[30] ;
     int num ;
 
     printf("이름을 입력하시오:  "); scanf("%s", name);
     printf("학번을 입력하시오:  "); scanf("%d", &num);
+    printf("팀 이름을 입력하시오 : "); scanf("%s", team_name);
 
     printf("\n<출력> \n");
     printf("이름 : %s\n", name);
-    printf("학번 : %d", num);
+    printf("학번 : %d\n", num);
+    printf("팀 이름 : %s\n", team_name);`
     return 0;
 }
 
